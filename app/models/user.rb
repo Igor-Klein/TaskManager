69495d87040e242a97be4e7f21777291909d27c8
Igor Klein <89021282507@mail.ru>
2019-11-28 18:01:04 +0400

    Communications and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :surname, presence: true
+  validates :surname, length: { minimum: 2 }
+  validates :email, presence: true
+  validates :email, format: { with: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/ }
+  validates :email, uniqueness: true
 end
