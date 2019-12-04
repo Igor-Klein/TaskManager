d8532ac77c66bce0b5f0662230cb79cd4101996e
Igor Klein <89021282507@mail.ru>
2019-12-04 13:15:11 +0400

    create user, developers

--- test/factories/sequences.rb
+++ test/factories/sequences.rb
@@ -4,13 +4,10 @@ FactoryBot.define do
   sequence :email do |n|
     "email#{n}@factory.com"
   end
-  sequence :first_name, aliases: %i[last_name password avatar name description] do |n|
+  sequence :first_name, aliases: %i[last_name string password avatar name description] do |n|
     "string#{n}"
   end
   sequence :expired_at do |_n|
     Time.now + rand(10)
   end
-  sequence :string do |n|
-    "string#{n}"
-  end
-end 
+end
