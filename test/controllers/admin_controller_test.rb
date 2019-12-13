6ef1ecc2c853514b772abaa5e54158cb960c3fda
Igor Klein <89021282507@mail.ru>
2019-12-13 15:20:11 +0400

    created Authorization

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  test 'should get show' do
+  setup do
+    admin = create(:admin)
+    sign_in_as admin
+  end
+
+  test "should get show" do
     user = create(:user)
     get admin_user_url(user)
     assert_response :success
