324a1971dd439213ce8d9cfaa01a6efb95be995d
Igor Klein <89021282507@mail.ru>
2019-12-13 16:12:34 +0400

    edit and new user

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -5,4 +5,15 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     get admin_users_url
     assert_response :success
   end
+
+  test 'should get new' do
+    get new_admin_user_url
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    user = create(:user)
+    get edit_admin_user_url user.id
+    assert_response :success
+  end
 end
