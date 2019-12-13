9d2331b709711819fcb2426769f5f116114b4230
Igor Klein <89021282507@mail.ru>
2019-12-13 16:00:27 +0400

    created index

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,12 +1,11 @@
 class Admin::ApplicationController < ApplicationController
   include AuthHelper
+
+  before_action :authenticate_user!, :authorize
   helper_method :current_user
 
-  
-  before_action :authenticate_user!, :authorize
-
   def authorize
-    render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false) if (forbidden?)
+    render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
   end
 
   def forbidden?
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -6,7 +6,7 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     sign_in_as admin
   end
 
-  test "should get show" do
+  test 'should get show' do
     user = create(:user)
     get admin_user_url(user)
     assert_response :success
