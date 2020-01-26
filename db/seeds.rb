5790272550c6ddae72f617d47165959b8469832d
Igor Klein <89021282507@mail.ru>
2020-01-26 14:17:40 +0400

    add Tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@ admin.save
   u.password = i.to_s
   u.save
 end
+
+# Generate microposts for a subset of users.
+users = Developer.all
+20.times do |i|
+  state = "in_development" if i % 2 == 0
+  state = "in_qa" if i % 3 == 0
+  state = "in_code_review" if i % 5 == 0
+  state = "ready_for_release" if i % 10 == 0
+  users.each do |user| 
+    Task.create(author: user, name: "Test_#{i}", description: "test_#{i}", state: state)
+  end 
+end
