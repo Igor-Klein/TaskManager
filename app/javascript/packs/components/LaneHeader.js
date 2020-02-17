ce265e58c7649801de74a7aa640e91c516f54234
Igor Klein <89021282507@mail.ru>
2020-02-17 17:24:47 +0400

    fix 5

--- app/javascript/packs/components/LaneHeader.js
+++ app/javascript/packs/components/LaneHeader.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class LaneHeader extends React.Component {
-  render() {
-    const { id, cards, totalCount } = this.props
-    return (
-      <div>
-        <b>{id}</b> ({cards.length}/{totalCount})
-      </div>
-    )
-  }
-}
+const LaneHeader = ({ id, cards, totalCount }) => (
+  <div>
+    <b>{id}</b> ({cards.length}/{totalCount})
+  </div>
+)
 
 LaneHeader.propTypes = {
   id: PropTypes.string.isRequired,
   cards: PropTypes.array.isRequired,
   totalCount: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
 }
+
+export default LaneHeader
